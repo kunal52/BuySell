de1e58d7ed83ed257cab1468d071eb57dcd37276
kunal52 <purikunal22@gmail.com>
2018-03-09 23:44:22 +0530

    Edit Usessl and verifyServerCertificate in JDBC link

--- src/main/java/database/DatabaseCredentials.java
+++ src/main/java/database/DatabaseCredentials.java
@@ -2,7 +2,7 @@ package database;
 
 public interface DatabaseCredentials {
 	
-	String url = "jdbc:mysql://localhost:3306/reutilize";
+	String url = "jdbc:mysql://localhost:3306/reutilize?verifyServerCertificate=false&useSSL=true";
 	String username = "root";
 	String password = "spider";
 	String password2="GQDQ8qa5BAHo";
